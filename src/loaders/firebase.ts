9aed19a66eb4db36f44e8e9ba47eeb3470f7524d
2tle <iam@2tle.io>
2024-01-25 12:18:20 +0900

    fin

--- src/loaders/firebase.ts
+++ src/loaders/firebase.ts
@@ -10,7 +10,8 @@ export default async() => {
             universe_domain: "googleapis.com",
             project_id: process.env.PROJECT_ID,
             private_key_id: process.env.PRIVATE_KEY_ID,
-            private_key: process.env.PRIVATE_KEY,
+            //@ts-ignore
+            private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
             client_email: process.env.CLIENT_EMAIL,
             client_id: process.env.CLIENT_ID,
             client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
