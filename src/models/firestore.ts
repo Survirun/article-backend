dd789fc36a7decbc27d4d762848116bcaae0c798
Hyunjun Yang <iam@2tle.io>
2024-02-03 16:50:53 +0900

    Update firestore.ts

--- src/models/firestore.ts
+++ src/models/firestore.ts
@@ -15,12 +15,12 @@ interface Article {
 
 export default {
     getArticle: async(keywords: Array<number>) => {
-        const rawData = await getFirestore().collection("web").where("category", 'in', keywords).where("cx","==",0).limit(30).get()
+        const rawData = await getFirestore().collection("web").where("category", 'in', keywords).limit(30).get()
         const data = rawData.docs.map(doc => doc.data() as Article)
         return data
     },
     getArticlesByDocId: async (articleIds: Array<string>) => {
-        const rawData = await getFirestore().collection("web").where(FieldPath.documentId(), 'in', articleIds).where("cx","==",0).get()
+        const rawData = await getFirestore().collection("web").where(FieldPath.documentId(), 'in', articleIds).get()
         const data = rawData.docs.map(doc => doc.data() as Article)
         return data
     }
